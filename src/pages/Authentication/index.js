0eccc47f98f5ed73810b2cc77e24196d54b78aee
Ashan <aperera@mitrai.com>
2022-03-04 10:33:48 +0530

    Sign in / sign up UI

--- src/pages/Authentication/index.js
+++ src/pages/Authentication/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import { Nav, NavItem, NavLink, TabContent, TabPane, Container, Row, Col } from 'reactstrap';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 
@@ -22,22 +22,39 @@ const Authentication = () => {
   }
   return (
     <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink className={activeTab == 'sign-in' ? 'active' : ''} onClick={() => toggleTab('sign-in')}>
-            SIGN IN
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className={activeTab == 'sign-up' ? 'active' : ''} onClick={() => toggleTab('sign-up')}>
-            SIGN UP
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane title='SIGN IN' tabId="sign-in"><SignIn /></TabPane>
-        <TabPane title='SIGN UP' tabId="sign-up"><SignUp /></TabPane>
-      </TabContent>
+      <Container fluid className="p-0">
+        <Row className="g-0">
+          <Col xl={6}>
+            <div className="auth-full-bg pt-lg-5 p-4">
+              <div className="w-100">
+                <div className="bg-overlay"></div>
+              </div>
+            </div>
+          </Col>
+          <Col xl={6}>
+            <div className="auth-full-page-content p-md-5 p-4">
+              <div className="w-100">
+                <Nav tabs>
+                  <NavItem>
+                    <NavLink className={activeTab == 'sign-in' ? 'active' : ''} onClick={() => toggleTab('sign-in')}>
+                      SIGN IN
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className={activeTab == 'sign-up' ? 'active' : ''} onClick={() => toggleTab('sign-up')}>
+                      SIGN UP
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+                <TabContent activeTab={activeTab}>
+                  <TabPane title='SIGN IN' tabId="sign-in"><SignIn /></TabPane>
+                  <TabPane title='SIGN UP' tabId="sign-up"><SignUp /></TabPane>
+                </TabContent>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
