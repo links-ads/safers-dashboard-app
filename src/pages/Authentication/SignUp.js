2ebb7554a9d099d7101f1897782dbba87b52accb
Ashan <aperera@mitrai.com>
2022-03-07 18:44:20 +0530

    Conflict resolve

--- src/pages/Authentication/SignUp.js
+++ src/pages/Authentication/SignUp.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik } from 'formik';
-import { Button, Col, Form, FormGroup, Input, InputGroup, InputGroupText, Label, Row, } from 'reactstrap';
+import { Button, Col, Form, FormGroup, Input, InputGroup, InputGroupText, Label, Row, Progress, List } from 'reactstrap';
 import { Link, useNavigate } from 'react-router-dom';
-import PasswordChecklist from 'react-password-checklist'
-import PasswordStrengthBar from 'react-password-strength-bar';
 import * as Yup from 'yup'
 import { signUp as registration } from '../../store/appAction';
 import { organisations, roles } from '../../constants/dropdowns';
@@ -41,6 +39,76 @@ const SignUp = () => {
       .oneOf([true], 'Please accept the terms and conditions')
   });
 
+  const pswStrengthIndicator = (password) => {
+    let chkUpperCase = false, chkLowerCase = false, hasNumber = false, chkLength = false, pswStrengthColor = 'Secondary';
+    let strengthScore = 0, pswStrength = 'Weak';
+
+    if(password.length > 7) {
+      strengthScore++;
+      chkLength = true;
+    }
+
+    if(password.match(/(?=.*[A-Z])/)){
+      strengthScore++;
+      chkUpperCase = true;
+    }
+    if(password.match(/(?=.*[a-z])/)){
+      strengthScore++;
+      chkLowerCase = true;
+    }
+    if(password.match(/(?=.*[0-9])/)){
+      strengthScore++
+      hasNumber = true;
+    }
+
+    switch(strengthScore) {
+    case 1:
+    case 2:
+      pswStrengthColor = 'danger';
+      break;
+    case 3:
+      pswStrengthColor = 'warning';
+      pswStrength = 'Average';
+      break;
+    case 4:
+      pswStrengthColor = 'success';
+      pswStrength = 'Strong';
+      break;
+    default:
+      pswStrengthColor = 'Secondary';
+      pswStrength = 'Weak';
+
+    }
+
+    const iconClass = 'float-start fs-4 fw-bold me-1';
+    const successIcon = 'bx bx-check-circle text-success';
+    const errorIcon = 'bx bx-x-circle text-danger';
+    return (<>
+      <Row className='mt-2'>
+        <Col>
+          <Progress id="pswStrength" multi width={25}>
+            <Progress bar color={strengthScore > 0 ? pswStrengthColor : ''} value={25} className="rounded grey" />
+            <Progress bar color={strengthScore > 1 ? pswStrengthColor : ''} value={25} className="ms-2 rounded grey" />
+            <Progress bar color={strengthScore > 2 ? pswStrengthColor : ''} value={25} className="ms-2 rounded grey" />
+            <Progress bar color={strengthScore > 3 ? pswStrengthColor : ''} value={25} className="ms-2 rounded grey" />
+          </Progress>
+        </Col>
+        <Col>
+          <span color={pswStrengthColor} className="float-end">{pswStrength}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <List type="unstyled" className='mt-3'>
+            <li className='mb-1'><i className={`${iconClass} ${chkLength ? successIcon : errorIcon}`}></i>8 characters long</li>
+            <li className='mb-1'><i className={`${iconClass} ${chkUpperCase ? successIcon :errorIcon}`}></i>Uppercase letter</li>
+            <li className='mb-1'><i className={`${iconClass} ${chkLowerCase ? successIcon : errorIcon}`}></i>Lowercase letter</li>
+            <li className='mb-1'><i className={`${iconClass} ${hasNumber ? successIcon : errorIcon}`}></i>Must contain number</li>
+          </List>
+        </Col>
+      </Row>
+    </>)
+  }
   return (
     <Formik
       initialValues={{
@@ -150,20 +218,7 @@ const SignUp = () => {
                         <i onClick={() => { setPasswordToggle(!passwordToggle) }} className={`fa ${passwordToggle ? 'fa-eye-slash' : 'fa-eye'}`} />
                       </InputGroupText>
                     </InputGroup>
-                    {values.password && (<>
-                      <PasswordStrengthBar password={values.password} />
-                      <PasswordChecklist
-                        rules={['minLength', 'capital', 'lowercase', 'number']}
-                        minLength={8}
-                        value={values.password}
-                        messages={{
-                          minLength: '8 characters long',
-                          capital: 'Uppercase letter',
-                          lowercase: 'Lowercase letter',
-                          number: 'Must contain number'
-                        }}
-                      />
-                    </>)}
+                    {values.password && (<>{pswStrengthIndicator(values.password)}</>)}
                     {errors.password && touched.password && (<div className="invalid-feedback">{errors.password}</div>)}
                   </FormGroup>
                 </Col>
