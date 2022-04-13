a0de833bff2858a4b89bd598cef8e44daf5aad11
Ashan <aperera@mitrai.com>
2022-04-13 14:23:37 +0530

    Media Pagination

--- src/pages/Reports/Dashboard/Containers/MediaContainer.js
+++ src/pages/Reports/Dashboard/Containers/MediaContainer.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types'
 import { Card, Row, Col } from 'reactstrap';
 import MediaComponent from '../../../../components/MediaComponent';
+import PaginationWrapper from '../../../../components/Pagination';
 
 const MediaContainer = ({ reportDetail }) => {
+
+  const [pageData, setPageData] = useState([]);
+
   if(!reportDetail)
     return null;
 
@@ -15,12 +19,15 @@ const MediaContainer = ({ reportDetail }) => {
             <span className='weather-text'>Media Files Attached</span>
           </Row>
           <Row >
-            {reportDetail.media.map((media, index) => {
+            {pageData.map((media, index) => {
               return( 
                 <Col key={index} md={3} sm={6} xs={12} className='d-flex dashboard-image justify-content-center'>
                   <MediaComponent media={media}/>
                 </Col>)
             })}
+          </Row>
+          <Row className='text-center'>
+            <PaginationWrapper pageSize={8} list={reportDetail.media} setPageData={setPageData} />
           </Row>
         </Card>
       </Col>
