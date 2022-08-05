7548e506a7c5f42b9a999e0065da9b62c768a3bc
stevefaeembra <stevendkay@gmail.com>
2022-08-05 11:47:40 +0100

    feat(frontend): Add temporary code
    Added code to put some mock data in leaf nodes, pending
    API providing this data for us
    
    IssueID SAFB-195

--- src/pages/DataLayer/OnDemandTreeView.js
+++ src/pages/DataLayer/OnDemandTreeView.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
 import { ListGroup, ListGroupItem, Collapse } from 'reactstrap';
 import { fetchEndpoint } from '../../helpers/apiHelper';
+import _ from 'lodash';
 
 const PropsPanel = (node) => {
   const node2=node.node;
@@ -42,6 +43,40 @@ const OnDemandTreeView = ({ data, setCurrentLayer}) => {
     }));
   }
 
+
+  // TODO this is a temporary thing to let me continue working while
+  // waiting on the API to be developed. It looks up the data layer name from
+  // the code, and injects a random status
+
+  const MOCK_LEAF_NODE = (node) => {
+    const lookup_table = [
+      {id: '36004', name:'Impact quantification'},
+      {id: '36005', name:'Fire front and smoke'},
+      {id: '36003', name:'Burned area geospatial image'},
+      {id: '36002', name:'Burned area severity map'},
+      {id: '36001', name:'Burned area delineation map'},
+      {id: '37006', name: 'Generate vegetation recovery map'},
+      {id: '37005', name: 'Generate historical severity map (dNBR)'},
+      {id: '37004', name: 'Provide landslide susceptibility information'},
+      {id: '37003', name: 'Generate soil recovery map (Vegetation Index)'},
+      {id: '37002', name: 'Generate burn severity map (dNBR)'},
+      {id: '32005', name: 'Get critical points of infrastructure, e.g. airports, motorways, hospitals, etc.'},
+      {id: '35006', name:'Fire Simulation'},
+      {id: '35011', name:'Max rate of spread'},
+      {id: '35010', name:'Mean rate of spread'},
+      {id: '35009', name:'Max fireline intensity'},
+      {id: '35008', name:'Mean fireline intensity'},
+      {id: '35007', name:'Fire perimeter simulation as isochrones maps'},
+    ];
+    let name = lookup_table.find(item=>item.id === node.datatype_id);
+    if (name) {
+      name=name.name;
+    }
+    const statuses = ['IN PROGRESS', 'FAILED', 'SUCCESS']
+    const status = statuses[_.random(0,2)];
+    return {name, status};
+  }
+
   const mapper = (nodes, parentId, lvl) => {
     return nodes?.map((node, index) => {
 
@@ -50,14 +85,17 @@ const OnDemandTreeView = ({ data, setCurrentLayer}) => {
         node.children= node?.layers || node?.requests || undefined;
       }
       
+      const mockleafnode = MOCK_LEAF_NODE(node);
+
       // use tree level to define main text
       const nodeTextByLevel = [
         `${node.key} : ${node.category}`,
         `${node.key} : ${node.title || node.id}`,
-        `${node.key} : ${node.datatype} [${node.status}]`
+        //`${node.key} : ${node.datatype} [${node.status}]`
+        `${node.key} : ${node.datatype_id}: ${mockleafnode.name} [${mockleafnode.status}]`
       ]
       node.text = nodeTextByLevel[lvl];
-      console.log(`Node ${node.key} : ${node.text}`)
+      console.log('Node', node);
       node.info = 'I\'m a tooltip';
 
       const id = node.id ?? node.key;
