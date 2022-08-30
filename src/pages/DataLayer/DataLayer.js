1dc8f4d2366af0af934c58b3342ec9dfebea997d
Rory MacGregor <rory.macgregor@astrosat.net>
2022-08-30 16:21:12 +0100

    fix(frontend): Upgrade searchbar
    Upgraded the searchbar functionality to drill down all layers to search for matches, instead
    of just the parent node.

--- src/pages/DataLayer/DataLayer.js
+++ src/pages/DataLayer/DataLayer.js
@@ -44,9 +44,10 @@ const DataLayer = ({
   bitmapLayer,
   viewState,
   timestamp,
-  dispatch,
   timeSeriesData,
-  featureInfoData
+  featureInfoData,
+  getAllTitles,
+  dispatch
 }) => {
   const [searchedDataLayers, setSearchedDataLayers] = useState(null);
 
@@ -68,7 +69,12 @@ const DataLayer = ({
     if (!value) setSearchedDataLayers(operationalMapLayers);
   
     const searchResult = operationalMapLayers.filter(
-      layer => layer.text.toLowerCase().includes(value.toLowerCase())
+      layer => {
+        const titles = getAllTitles([layer]);
+        return titles.some(
+          str => str.toLowerCase().includes(value.toLowerCase())
+        )
+      }
     );
   
     setSearchedDataLayers(searchResult);
@@ -438,8 +444,12 @@ DataLayer.propTypes = {
   bitmapLayer: PropTypes.any,
   viewState: PropTypes.any,
   timestamp: PropTypes.string,
+<<<<<<< HEAD
   timeSeriesData: PropTypes.any,
   featureInfoData: PropTypes.any,
+=======
+  getAllTitles: PropTypes.func,
+>>>>>>> fix(frontend): Upgrade searchbar
   dispatch: PropTypes.func
 }
 
