96d718fc8d91abbf2c801864e448a5d51e08446f
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-09-23 12:29:01 +0100

    feat(frontend): changed communication field that is used for source organization

--- src/pages/Chatbot/Comms/Components/Comm.js
+++ src/pages/Chatbot/Comms/Components/Comm.js
@@ -66,7 +66,7 @@ const Comm = ({ card, commID, setSelectedComm }) => {
               <Col md={3}  className='d-flex align-items-end'>
                 <CardText>
                   <span className='float-end alert-source-text me-2 no-wrap'>
-                    {card.assigned_to.join('')}
+                    {card.source_organization}
                   </span>
                 </CardText>
               </Col>
