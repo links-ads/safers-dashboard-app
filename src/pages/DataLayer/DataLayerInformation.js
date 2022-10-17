b64a7f3358ffb8365260a7a6d92f7d608989d66e
Mark Small <mark.small@astrosat.net>
2022-10-17 11:07:04 +0100

    fix(frontend): Only show GetTimeSeriesInfo option if there are any
    The `currentLayer` has the URL to use to get timeseries info, so
    instead of always disabling the option for on-demand layers, we now
    check to see if the layer data has a URL to be used, if yes, then
    allow this feature, otherwise hide it.

--- src/pages/DataLayer/DataLayerInformation.js
+++ src/pages/DataLayer/DataLayerInformation.js
@@ -23,7 +23,6 @@ const DataLayerInformationComponent = ({
   tempLayerData,
   setTempLayerData,
   setInformation,
-  featureOnly = false,
   currentViewState,
   dispatch
 }) => {
@@ -40,6 +39,7 @@ const DataLayerInformationComponent = ({
   const [selectedDomain, setSelectedDomain] = useState(null);
   const [contextMenuKey, setContextMenuKey] = useState(0);
   const chartContainerRef = useRef(null);
+  const timeseriesExists = !!currentLayer?.timeseries_url;
 
   let title = currentLayer?.title
   if (currentLayer?.units) {
@@ -110,7 +110,7 @@ const DataLayerInformationComponent = ({
         </div>
       </Card>}
 
-      {layerData && !featureOnly &&
+      {layerData && timeseriesExists &&
         <div ref={chartContainerRef}>
           {chartValues?.length > 0 && <Card color="dark default-panel mt-3">
             <h4 className='ps-3 pt-3 mb-2'><i className='meta-close' onClick={clearInfo}>x</i></h4>
@@ -326,7 +326,7 @@ const DataLayerInformationComponent = ({
       {currentLayer && <><MenuItem className="geo-menuItem" onClick={toggleDisplayLayerInfo}>
         Get Feature Info
       </MenuItem>
-      {!featureOnly && <MenuItem className="geo-menuItem" onClick={toggleTimeSeriesChart}>
+      {timeseriesExists && <MenuItem className="geo-menuItem" onClick={toggleTimeSeriesChart}>
           Time Series Chart
       </MenuItem>}</>}
     </ContextMenu>
@@ -340,7 +340,6 @@ DataLayerInformationComponent.propTypes = {
   tempLayerData: PropTypes.any,
   setTempLayerData: PropTypes.any,
   setInformation: PropTypes.any,
-  featureOnly: PropTypes.bool,
   currentViewState: PropTypes.any,
   dispatch: PropTypes.any,
 }
