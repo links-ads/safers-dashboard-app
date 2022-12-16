e71efcdeae222992f6b1fe232bae5cd156273f4b
stevefaeembra <stevendkay@gmail.com>
2022-12-16 16:25:32 +0000

    feat(frontend): Missed one from last commit
    Describe why you did what you did
    
    IssueID #

--- src/pages/NewDashboard/index.js
+++ src/pages/NewDashboard/index.js
@@ -3,6 +3,7 @@ import { Container } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { getAllEventAlerts } from '../../store/events/action';
 import NotificationsBar from './Components/Notifications';
+import AOIBar from './Components/AOIBar';
 
 const NewDashboard = () => {
   const dispatch = useDispatch();
@@ -16,6 +17,7 @@ const NewDashboard = () => {
       <Container fluid className="sign-up-aoi-map-bg">
         <p className="alert-title">General Dashboard</p>
         <NotificationsBar />
+        <AOIBar />
       </Container>
     </div>
   );
