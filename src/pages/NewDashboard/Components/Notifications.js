e592d1f4acd6b58cea86b4ddabf8d4dfe4ec45bb
stevefaeembra <stevendkay@gmail.com>
2022-12-20 13:31:37 +0000

    feat(frontend): Implemented People card
    Very similar to what I did with the Event card
    
    IssueID SAFB-119

--- src/pages/NewDashboard/Components/Notifications.js
+++ src/pages/NewDashboard/Components/Notifications.js
@@ -3,20 +3,25 @@ import { Container, Row } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllEventAlerts } from '../../../store/events/action';
 import NotificationCard from './NotificationCard';
+import { getAllPeople } from '../../../store/people/action';
 //import { set } from 'lodash';
 
 const NotificationsBar = () => {
   const dispatch = useDispatch();
   const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
-  let [activityStatusCounts, setActivityStatusCounts] = useState({undefined: 0});
-  const defaultAoi = useSelector(state => state?.user?.defaultAoi);
-  const { allAlerts: alerts, filteredAlerts } = useSelector(state => state?.alerts);
-  
-  console.log('defaultAOI', defaultAoi);
+  let [activityStatusCounts, setActivityStatusCounts] = useState( {undefined: 0} );
+  let [peopleStatusCounts, setPeopleStatusCounts] = useState( {undefined: 0} );
 
-  console.log('alerts', alerts);
-  console.log('filteredalerts', filteredAlerts);
+  const defaultAOI = useSelector(state => state?.user?.defaultAoi);
+  const { allAlerts: alerts, filteredAlerts } = useSelector(state => state?.alerts);
+  const { allPeople: orgPplList, filteredPeople } = useSelector(state => state.people);
+
+  console.log('Alerts!', alerts);
+  console.log('orgPplList', orgPplList);
+  console.log('filteredPeople', filteredPeople);
+
+  const nameOfAOI = defaultAOI?.features[0]?.properties?.name;
 
   const getAlertCountByStatus = (alerts) => {
     // build an object whose keys are alert types and values are
@@ -29,9 +34,23 @@ const NotificationsBar = () => {
         statusCounts[alert.status] = 1;
       }
     })
-    console.log('Status Counts', statusCounts);
+    console.log('statusCounts', statusCounts);
     return statusCounts;
   };
+
+
+  const getPersonCountByStatus = (people) => {
+    let statusCounts = {};
+    people.forEach( person=> {
+      if (person?.status in statusCounts) {
+        statusCounts[person.status] += 1;
+      } else {
+        statusCounts[person.status] = 1;
+      }
+    })
+    console.log('statusCounts', statusCounts);
+    return statusCounts;
+  }
 
   // renderer functions. These are passed to the individual NotificationCards
   // These should return some JSX which will be rendered as the contents
@@ -42,18 +61,33 @@ const NotificationsBar = () => {
   };
 
   const renderActivities = () => {
-    if (!activityStatusCounts) {
-      console.log('bugging out');
-      return null;
+    if (!activityStatusCounts || Object.keys(activityStatusCounts).length===0 ) {
+      return <h3>No new Event activity</h3>;
     }
-    Object.keys(activityStatusCounts).forEach(key => console.log('Status count', `${key} : ${activityStatusCounts[key]}`))
     return (
       <>
         {
           Object.keys(activityStatusCounts).map(key => 
-            <h1 key={`item_${key}`}>
+            <h3 key={`item_${key}`}>
               {`${key} : ${activityStatusCounts[key]}`}
-            </h1>
+            </h3>
+          )
+        }
+      </>
+    );
+  }
+
+  const renderPeople = () => {
+    if (!peopleStatusCounts || Object.keys(peopleStatusCounts).length===0 ) {
+      return <h3>No new People activity</h3>;
+    }
+    return (
+      <>
+        {
+          Object.keys(peopleStatusCounts).map(key => 
+            <h3 key={`item_${key}`}>
+              {`${key} : ${peopleStatusCounts[key]}`}
+            </h3>
           )
         }
       </>
@@ -64,17 +98,31 @@ const NotificationsBar = () => {
 
   useEffect(() => {
     dispatch(getAllEventAlerts());
+    const params = {
+      bbox: undefined,
+      default_date: false,
+      default_bbox: false,
+    };
+    const feFilters = {};
+    dispatch(getAllPeople(params, feFilters));
   }, []);
 
   useEffect(() => {
+    console.log('received new alerts');
     const statusCounts = getAlertCountByStatus(alerts);
     setActivityStatusCounts(statusCounts);
   }, [filteredAlerts, alerts]);
 
+  useEffect(()=> {
+    console.log('received new people');
+    const statusCounts = getPersonCountByStatus(orgPplList);
+    setPeopleStatusCounts(statusCounts);
+  }, [orgPplList, filteredPeople])
+
   return (
     <div className="">
       <Container fluid className="">
-        <p className="align-self-baseline alert-title">Dashboard</p>
+        <p className="align-self-baseline alert-title">Area of interest: { nameOfAOI }</p>
         <Row className="gx-1 row-cols-5">
           <NotificationCard 
             cardName="Fire Alerts" 
@@ -85,7 +133,7 @@ const NotificationsBar = () => {
           <NotificationCard 
             cardName="People" 
             iconClass="fas fa-user-alt" 
-            contentRenderer={ renderLorem }
+            contentRenderer={ renderPeople }
             linkURL="/chatbot?tab=1"
           />
           <NotificationCard 
