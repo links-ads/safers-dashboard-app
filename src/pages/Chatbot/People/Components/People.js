3de96d57263005af98b0f4b26cdc806b551e91cb
stevefaeembra <stevendkay@gmail.com>
2023-01-04 15:19:32 +0000

    feat(frontend): Fix chatbot module
    Prevent crashes when we don't get a location back from the API
    
    IssueID SAFB-119

--- src/pages/Chatbot/People/Components/People.js
+++ src/pages/Chatbot/People/Components/People.js
@@ -32,26 +32,26 @@ const People = ({ card, peopleId, setSelectedPeople/*, setFavorite*/ }) => {
             <Row>
               <Col>
                 <CardTitle>
-                  <span className='card-title'>{card.username}</span>
+                  <span className='card-title'>{card?.username}</span>
                 </CardTitle>
                 <CardText className='card-desc'>
                   {t('status', {ns: 'common'})}: {getBadge()}
                 </CardText>
                 <CardText className='card-desc'>
-                  {t('activity', {ns: 'common'})}: {card.activity}
+                  {t('activity', {ns: 'common'})}: {card?.activity}
                 </CardText>
               </Col>
             </Row>
             <Row className='mt-0'>
               <Col>
                 <p className="text-muted no-wrap text-capitalize mb-1">
-                  {t('Location' , {ns: 'common'})}: {(card.location).join(', ')}
+                  {t('Location' , {ns: 'common'})}: {(card?.location) ? (card.location).join(', '): ''}
                 </p>
               </Col>
               <Col>
                 <CardText>
                   <span className='float-end alert-source-text me-2 mb-1'>
-                    {t('last-updated')}: {formatDate(card.timestamp)}
+                    {t('last-updated')}: {formatDate(card?.timestamp)}
                   </span>
                 </CardText>
               </Col>
