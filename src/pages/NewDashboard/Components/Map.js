a22aa34e9cf6cb328ccfcec7d4b837f095174088
stevefaeembra <stevendkay@gmail.com>
2023-01-09 11:21:36 +0000

    feat(frontend): Display AOI polygon on dashboard map
    Rescued code from old dashboard which had this
    Reinstated access to old dashboard to prepare
    for a demo
    
    IssueID SAFB-119

--- src/pages/NewDashboard/Components/Map.js
+++ src/pages/NewDashboard/Components/Map.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Card, Row } from 'reactstrap';
-
+import { getAllAreas } from '../../../store/appAction'
 import BaseMap from '../../../components/BaseMap/BaseMap';
+import { getPolygonLayer, getViewState } from '../../../helpers/mapHelper';
+import _ from 'lodash';
+import { setPolygonLayer, setSelectedAoi, setViewState } from '../../../store/common/action';
 
 const MapComponent = () => {
   const { polygonLayer, viewState } = useSelector(state => state.common);
+  const dispatch = useDispatch()
+  const defaultAoi = useSelector(state => state.user.defaultAoi);
+
+  const {aois: allAoi} = useSelector(
+    state => state.common
+  )
+
+  useEffect(() => {
+    if(!allAoi.length){
+      dispatch(getAllAreas());
+    }
+  }, []);
+
+  const setMapLayers = (objAoi) => {
+    if(objAoi){
+      dispatch(setPolygonLayer(getPolygonLayer(objAoi)));
+      dispatch(setViewState(getViewState(objAoi.features[0].properties.midPoint, objAoi.features[0].properties.zoomLevel)))
+    }
+  }
+
+  const selectAoi = (id) => {
+    const objAoi = _.find(allAoi, { features: [{ properties: { id: parseInt(id) } }] })
+    dispatch(setSelectedAoi(parseInt(id)));
+    setMapLayers(objAoi)
+  }
+
+  useEffect(() => {
+    selectAoi(defaultAoi.features[0].properties.id)
+  }, [allAoi]);
+
   return (
     <Card className='map-card'>
       <Row style={{ height: 350 }} className="mb-5">
