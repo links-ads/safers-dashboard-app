7f2b66751fae0f8684a256b38e471b84e148a082
stevefaeembra <stevendkay@gmail.com>
2023-01-10 12:32:15 +0000

    feat(frontend): Added alerts mock
    In case I can't get any alerts to appear
    
    IssueID SAFB-119

---
+++ src/pages/NewDashboard/mocks/alertsmock.ts
@@ -0,0 +1,35 @@
+export const MOCK_ALERTS = [
+  [
+    {
+      id: '3a851a61-7f8a-4aa1-ad63-31e61b051a36',
+      title: 'Notification 3a851a61 [Fire]',
+      type: 'UNVALIDATED',
+      timestamp: '2022-01-10T14:29:34Z',
+      status: 'string',
+      source: 'string',
+      scope: 'string',
+      category: 'string',
+      event: 'string',
+      urgency: 'string',
+      severity: 'string',
+      certainty: 'string',
+      description: '',
+      geometry: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [42.0264, 9.0268],
+            },
+            properties: {},
+          },
+        ],
+      },
+      center: [9.2902, 41.5812],
+      bounding_box: [9.2902, 41.5812, 9.2902, 41.5812],
+      information: 'Lorem ipsum dolor sit amet',
+    },
+  ],
+];
