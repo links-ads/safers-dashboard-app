515b7f15dd27f18c407f0318af7f67ce3791fdd6
Mark Small <mark.small@astrosat.net>
2023-01-10 11:54:43 +0000

    feat(frontend): Add ability to switch the Map's basemap
    Until now we have only one basemap available to use, even though
    there were others defined. I've now added a MapStyleSwitcher which
    allows the user to select a different one. Whichever MapStyle is
    chosen, is applied to all maps in the application. The selected MapStyle
    is also written to localstorage, so that when you restart your browser,
    you get your previously selected basemap.

---
+++ src/components/BaseMap/MapStyleSwitcher.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+
+export const MapStyleSwitcher = ({
+  mapStyles = {},
+  // selectedMapStyle,
+  selectMapStyle,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <UncontrolledDropdown
+      className="me-2"
+      direction="down"
+    >
+      <DropdownToggle
+        caret
+        color="primary"
+      >
+        <span className="d-flex align-items-center">
+          <i className="bx bxs-map-alt p-2 map-style-icon"></i>
+          {t('Change Map Style', { ns: 'common' })}
+        </span>
+      </DropdownToggle>
+      <DropdownMenu>
+        {mapStyles.map(mapStyle => (
+          <div className="d-flex flex-column align-items-center justify-content-center" key={mapStyle.label}>
+            <DropdownItem onClick={() => selectMapStyle(mapStyle)}>
+              <div className="d-flex flex-column align-items-center justify-content-center">
+                <img src={mapStyle.thumbnail} className="w-75 h-75" alt={mapStyle.label} />
+                <p>{mapStyle.label}</p>
+              </div>
+            </DropdownItem>
+          </div>
+        ))}
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  );
+};
+
+MapStyleSwitcher.propTypes = {
+  mapStyles: PropTypes.arrayOf(PropTypes.object),
+  selectMapStyle: PropTypes.func,
+}
