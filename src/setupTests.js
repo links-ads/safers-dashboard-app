4bf0877258991f04852c790bd356749e5a20f641
Rory MacGregor <rory.macgregor@astrosat.net>
2023-02-17 17:37:23 +0000

    feat(frontend): Switch from Axios mocking to MSW
    * Installed MSW dependency and removed Axios and fetchMock
    * Configured MSW and setupTests
    * Removed old Axios mocking
    * Add proxy backend

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
+import '@testing-library/jest-dom';
+
+import { server } from 'mocks/server';
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
@@ -6,3 +10,30 @@
 window.scrollTo = (x, y) => {
   document.documentElement.scrollTop = y;
 };
+
+jest.mock('components/BaseMap/BaseMap', () => () => {
+  return <div />;
+});
+
+jest.mock('components/BaseMap/PolygonMap', () => () => {
+  return <div />;
+});
+
+// MSW
+// Establish API mocking before all tests.
+beforeAll(() => {
+  server.listen({
+    onUnhandledRequest(req) {
+      console.error(
+        'Found an unhandled %s request to %s',
+        req.method,
+        req.url.href,
+      );
+    },
+  });
+});
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
