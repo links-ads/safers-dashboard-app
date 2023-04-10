e3b43e1940e0236109b3a0e94f92d7f222035308
Mark Small <mark.small@astrosat.net>
2023-04-10 15:27:20 +0100

    feat(frontend): Add camera/image metadata to image
    I've added metadata about each camera image, right below each image.
    
    IssueID ORB-423

--- src/pages/Dashboard/Components/PhotoBar.js
+++ src/pages/Dashboard/Components/PhotoBar.js
@@ -1,22 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import { Img } from 'react-image';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Container, Card, Row } from 'reactstrap';
 
 import { fetchCameraAlerts, allInSituAlertsSelector } from 'store/insitu.slice';
+import { formatDate } from 'utility';
 
-import { ReactComponent as Placeholder } from './placeholder.svg';
+const NUMBER_OF_PHOTOS = 8;
 
 const PhotoBar = ({ t }) => {
-  const [photoList, setPhotoList] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const NUMBER_OF_PHOTOS = 8;
-
   const allPhotos = useSelector(allInSituAlertsSelector);
 
   const dispatch = useDispatch();
@@ -34,21 +29,11 @@ const PhotoBar = ({ t }) => {
     );
   }, [dispatch]);
 
-  useEffect(() => {
-    setPhotoList(allPhotos);
-    setIsLoaded(true);
-  }, [allPhotos]);
-
   return (
     <Container
       fluid="true"
       className="mx-8 flex-stretch align-content-center flex-wrap"
     >
-      {!isLoaded ? (
-        <Card>
-          <h1>Loading...</h1>
-        </Card>
-      ) : null}
       <Card className="card">
         <Row className="align-self-baseline alert-title mx-3">
           <Link to="/insitu-alerts">
@@ -59,30 +44,24 @@ const PhotoBar = ({ t }) => {
           </Link>
         </Row>
         <Row xs={1} sm={2} md={3} lg={4}>
-          {photoList && photoList.length === 0 ? (
+          {allPhotos.length === 0 ? (
             <div className="card noshadow mx-5">{t('No photos in AOI')}</div>
           ) : null}
-          {photoList && photoList.length > 0 ? (
-            photoList?.slice(0, NUMBER_OF_PHOTOS).map(photo => {
-              return (
-                <div key={`photie_${photo.id}`} className="p-3 my-3">
-                  {/* This is a custom component which handles fallbacks */}
-                  <Img
-                    decode={true}
-                    unloader={<Placeholder width="100%" height="120px" />}
-                    src={[`${photo.url}`]}
-                    width="100%"
-                    height="120px"
-                    alt="Image is missing"
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <div className="card noshadow w-100 mx-4 px-3">
-              <h3>Looking for photos...</h3>
-            </div>
-          )}
+          {allPhotos.slice(0, NUMBER_OF_PHOTOS).map(photo => {
+            return (
+              <div key={photo.id} className="p-3 my-3 ml-3">
+                <img
+                  src={photo.url}
+                  width="100%"
+                  height="120px"
+                  alt={`Camera ${photo.camera_id}`}
+                />
+
+                <div>{photo.camera_id}</div>
+                <div>{formatDate(photo.timestamp)}</div>
+              </div>
+            );
+          })}
         </Row>
       </Card>
     </Container>
