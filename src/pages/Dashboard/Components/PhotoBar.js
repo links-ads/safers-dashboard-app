f6e41bc5ea648f32de69f4aeca8a00ab74f2ceb5
Mark Small <mark.small@astrosat.net>
2023-04-10 15:32:38 +0100

    feat(frontend): Let user select camera to show images for
    I've added a select dropdown, so that the user can select which
    camera to show images for.
    
    IssueID SAFB-423

--- src/pages/Dashboard/Components/PhotoBar.js
+++ src/pages/Dashboard/Components/PhotoBar.js
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container, Card, Row } from 'reactstrap';
+import { Container, Card, Row, Col, Input } from 'reactstrap';
 
-import { fetchCameraAlerts, allInSituAlertsSelector } from 'store/insitu.slice';
+import {
+  fetchCameraAlerts,
+  fetchCameraSources,
+  allInSituAlertsSelector,
+  cameraSourcesSelector,
+} from 'store/insitu.slice';
 import { formatDate } from 'utility';
 
 const NUMBER_OF_PHOTOS = 8;
 
 const PhotoBar = ({ t }) => {
+  const [selectedCamera, setSelectedCamera] = useState('All');
+  const [photos, setPhotos] = useState([]);
+
+  const cameraSources = useSelector(cameraSourcesSelector);
   const allPhotos = useSelector(allInSituAlertsSelector);
 
   const dispatch = useDispatch();
@@ -27,7 +36,20 @@ const PhotoBar = ({ t }) => {
         default_bbox: true,
       }),
     );
+    dispatch(fetchCameraSources());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (selectedCamera === 'All') {
+      setPhotos(allPhotos.slice(0, NUMBER_OF_PHOTOS));
+    } else {
+      const filteredPhotos = allPhotos.filter(
+        photo => photo.camera_id === selectedCamera,
+      );
+
+      setPhotos(filteredPhotos.slice(0, NUMBER_OF_PHOTOS));
+    }
+  }, [allPhotos, selectedCamera]);
 
   return (
     <Container
@@ -43,11 +65,34 @@ const PhotoBar = ({ t }) => {
             </p>
           </Link>
         </Row>
-        <Row xs={1} sm={2} md={3} lg={4}>
-          {allPhotos.length === 0 ? (
+
+        <Row className="my-1 mx-4 row-cols-4">
+          <Col>
+            <Input
+              className="btn-sm sort-select-input mx-8"
+              name="inSutuSource"
+              placeholder="Source"
+              type="select"
+              onChange={event => setSelectedCamera(event.target.value)}
+              value={selectedCamera}
+            >
+              <option value="All">
+                --- {t('Source')} : {t('All')} ---
+              </option>
+              {cameraSources.map(camera => (
+                <option key={camera} value={camera}>
+                  {camera}
+                </option>
+              ))}
+            </Input>
+          </Col>
+        </Row>
+
+        <Row className="mx-3" xs={1} sm={2} md={3} lg={4}>
+          {photos.length === 0 ? (
             <div className="card noshadow mx-5">{t('No photos in AOI')}</div>
           ) : null}
-          {allPhotos.slice(0, NUMBER_OF_PHOTOS).map(photo => {
+          {photos.map(photo => {
             return (
               <div key={photo.id} className="p-3 my-3 ml-3">
                 <img
