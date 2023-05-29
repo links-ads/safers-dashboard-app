27e45860464d3531bf152cd5e1028cd139eced8d
Mark Small <mark.small@astrosat.net>
2023-05-29 20:50:28 +0100

    fix(frontend): Fix form validation
    The form is not validating correctly when a user selects `citizen`
    as their `role`. In this case, there should be no `organization`
    selected as that doesn't make sense, but the form could then not be
    submitted. I've now fixed the validation to only show the error if
    the role is not `citizen`.

--- src/pages/Authentication/SignUp.js
+++ src/pages/Authentication/SignUp.js
@@ -81,29 +81,25 @@ const SignUp = () => {
     }
   }, [navigate, signUpOauth2Success]);
 
-  const signUpSchema = Yup.object()
-    .shape({
-      email: Yup.string()
-        .email('Invalid email address')
-        .required('The field cannot be empty'),
-      password: Yup.string()
-        .matches(pwdRegEx, pwdValidationTxt)
-        .required('The field cannot be empty'),
-      first_name: Yup.string().required('The field cannot be empty'),
-      last_name: Yup.string().required('The field cannot be empty'),
-      role: Yup.string().required('The field cannot be empty'),
-      accepted_terms: Yup.bool().oneOf(
-        [true],
-        'Please accept the terms and conditions',
-      ),
-    })
-    .when((values, schema) => {
-      if (values.role !== citizenId) {
-        return schema.shape({
-          organization: Yup.string().required('The field cannot be empty'),
-        });
-      }
-    });
+  const signUpSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('The field cannot be empty'),
+    password: Yup.string()
+      .matches(pwdRegEx, pwdValidationTxt)
+      .required('The field cannot be empty'),
+    first_name: Yup.string().required('The field cannot be empty'),
+    last_name: Yup.string().required('The field cannot be empty'),
+    role: Yup.string().required('The field cannot be empty'),
+    accepted_terms: Yup.bool().oneOf(
+      [true],
+      'Please accept the terms and conditions',
+    ),
+    organization: Yup.string().when('role', {
+      is: val => val !== citizenId,
+      then: schema => schema.required('The field cannot be empty'),
+    }),
+  });
 
   const formSubmit = (values, setSubmitting) => {
     values.organization =
